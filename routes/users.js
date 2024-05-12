1e92346501685229dd298139730938561a0e9b5d
Ange <ange.ingabire07@gmail.com>
2024-05-12 23:16:18 +0200

    all changes

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 router.post('/', (req, res) => 
 {
@@ -22,5 +22,25 @@ router.get('/:id',(req, res ) =>
     const foundUser = users.find((user) => user.id === id)
     res.send(foundUser);
 })
+router.delete('/:id', (req, res) => 
+{
+    const { id } =req.params;
 
+    users = users.filter((user )=> user.id !== id)
+    res.send(`${id} deleted successfully in Database`);
+})
+
+router.patch('/:id', (req, res) =>
+{
+    const {id} = req.params;
+    const {firstname, lastname, email } = req.body;
+
+    const user = users.find((user) => user.id === id)
+
+    if(firstname) user.firstname = firstname;
+    if(lastname) user.lastname = lastname;
+    if(email) user. email = email;
+
+    res.send(`this ${id} has been updated successfully`);
+})
 export default router;
